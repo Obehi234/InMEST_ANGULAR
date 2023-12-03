ee19e543c371a1495edc6a57da0daff243070ddd
obehi234 <obehi.ataga234@gmail.com>
2023-12-03 22:41:23 +0100

    Created component for IT Support

---
+++ src/app/dashboard/it-support/it-support.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-it-support',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './it-support.component.html',
+  styleUrl: './it-support.component.scss'
+})
+export class ItSupportComponent {
+
+}
