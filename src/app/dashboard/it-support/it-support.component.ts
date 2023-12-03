0813ae1254b3fa0da3d65a57ba3d43199539f2f4
obehi234 <obehi.ataga234@gmail.com>
2023-12-03 23:08:15 +0100

    Created html for IT-Support component

--- src/app/dashboard/it-support/it-support.component.ts
+++ src/app/dashboard/it-support/it-support.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-it-support',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatCardModule, MatIconModule],
   templateUrl: './it-support.component.html',
   styleUrl: './it-support.component.scss'
 })
-export class ItSupportComponent {
+export class ItSupportComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  incidents: any[] = [];
+  
+  displayedColumns: string[] = ['id', 'eitID', 'state', 'description', 'category', 'action'];
+    dataSource = this.incidents
+  
 
 }
