e8a12f09e087373d2c3036203b895ac12cfa889d
Fleur <enondesessinou@gmail.com>
2023-12-03 14:35:16 +0100

    update button and card

--- src/app/dashboard/leave/leave.component.ts
+++ src/app/dashboard/leave/leave.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { LeaveService } from '../../services/leave.service';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MaterialModule } from './../../material/material.module';
+import { CdkTableDataSourceInput } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-leave',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterLink, MatTabsModule, MatTableModule, MatCardModule, MaterialModule],
   templateUrl: './leave.component.html',
-  styleUrl: './leave.component.scss'
+  styleUrl: './leave.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
+
 export class LeaveComponent implements OnInit {
   leaves: any [] = [];
+element: any;
+dataSource: CdkTableDataSourceInput<any> | undefined;
   constructor(
     private leaveService: LeaveService
   ){ }
