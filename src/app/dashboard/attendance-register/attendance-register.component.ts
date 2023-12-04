297d7fc1ad959ecca308452a30dc0c7b0cb93afc
Derek Addo <mr.derek.addo@gmail.com>
2023-12-04 15:40:48 +0000

    attendance inner component added

--- src/app/dashboard/attendance-register/attendance-register.component.ts
+++ src/app/dashboard/attendance-register/attendance-register.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AttendanceRegisterService } from '../../services/attendance-register/attendance-register.service';
 
 @Component({
-  selector: 'app-attendance-register',
+  selector: 'app-leaves',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './attendance-register.component.html',
   styleUrl: './attendance-register.component.scss'
 })
-export class AttendanceRegisterComponent {
 
+@Injectable()
+export class AttendanceRegisterComponent implements OnInit {
+  attendance: any[] = [];
+
+  constructor(
+    private attendanceRegisterService: AttendanceRegisterService
+  ){}
+
+ngOnInit(): void {
+  this.getAttendanceRecords();
+}
+  getAttendanceRecords() {
+    this.attendanceRegisterService.getAttendanceRecords().subscribe((resp: any) => {
+      this.attendance = resp;
+    });
+  }
 }
